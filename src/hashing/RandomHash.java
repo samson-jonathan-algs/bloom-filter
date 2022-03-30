0c8c67030e9704cdf5126a0d5df785f13b6a79d1
sracedorfman <83509848+sracedorfman@users.noreply.github.com>
2022-03-30 17:01:08 -0400

    basic hashing capability

---
+++ src/hashing/RandomHash.java
@@ -0,0 +1,30 @@
+package hashing;
+
+import java.util.Random;
+
+public class RandomHash {
+
+    private int range;
+    private int a;
+    private int b;
+    private double decider;
+
+    public RandomHash(int m, int n, int b) {
+        this.range = m*n*b;
+
+        Random rand = new Random();
+
+        this.a = rand.nextInt(100);
+        this.b = rand.nextInt(range);
+
+        this.decider = rand.nextDouble();
+
+    }
+
+    public int hash(int num) {
+        if (decider < 1) {
+            return ((num*a)+b)%range;
+        }
+        return 0;
+    }
+}
